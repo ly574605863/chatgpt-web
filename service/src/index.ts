140e018f4f824ccd28c635160c2ffe3c65a80de8
nickli <ly574605863@163.com>
2023-04-11 23:29:10 +0800

    feat: update wechat token

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto'
 import express from 'express'
 import type { RequestProps } from './types'
 import type { ChatMessage } from './chatgpt'
@@ -74,6 +75,26 @@ router.post('/verify', async (req, res) => {
       throw new Error('密钥无效 | Secret key is invalid')
 
     res.send({ status: 'Success', message: 'Verify successfully', data: null })
+  }
+  catch (error) {
+    res.send({ status: 'Fail', message: error.message, data: null })
+  }
+})
+
+router.get('/wx_token', async (req, res) => {
+  try {
+    const signature = req.query.signature
+    const timestamp = req.query.timestamp
+    const nonce = req.query.nonce
+    const echostr = req.query.echostr
+
+    const wx_token = '42AIDEV'
+    const str = [wx_token, timestamp, nonce].sort().join('')
+    const sha1 = crypto.createHash('sha1').update(str).digest('hex')
+    if (sha1 !== signature)
+      res.send({ status: 'Fail', message: 'token 验证失败', data: null })
+    else
+      res.send({ status: 'Success', message: 'error.message', data: echostr })
   }
   catch (error) {
     res.send({ status: 'Fail', message: error.message, data: null })
